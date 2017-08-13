bad81f541b19156cf76d23b1b69c010f9e90e2c0
jsergio123 <jsergio123@gmail.com>
2017-08-13 15:54:39 -0400

    Update gfsvideos.py

--- resources/plugins/gfsvideos.py
+++ resources/plugins/gfsvideos.py
@@ -15,40 +15,15 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
-import re, urlparse
-from urlresolver import common
-from urlresolver.plugins.lib import helpers
-from urlresolver.resolver import UrlResolver, ResolverError
+from urlresolver.plugins.__generic_resolver__ import GenericResolver
 
-class GFSVideosResolver(UrlResolver):
+class GFSVideosResolver(GenericResolver):
     name = 'gfsvideos'
     domains = ['gfsvideos.com']
-    pattern = '(?://|\.)(gfsvideos\.com)/video/([\w-]+-\d+\.html)'
-
-    def __init__(self):
-        self.net = common.Net()
-
-    def get_media_url(self, host, media_id):
-        headers = {'User-Agent': common.RAND_UA}
-        web_url = self.get_url(host, media_id)
-        html = self.net.http_GET(web_url, headers=headers).content
-
-        if html:
-            try:
-                file = re.search('''<source\s*src=['"]([^"']+)''', html, re.DOTALL).groups()[0]
-                headers.update({'Referer': web_url, \
-                                'Accept-Encoding': 'identity;q=1, *;q=0', \
-                                'chrome-proxy': 'frfr', \
-                                'Accept': '*/*', \
-                                'Accept-Language': 'en-US,en;q=0.8'})
-                return file + helpers.append_headers(headers)
-            except:
-                raise ResolverError('File not found')
-
-        raise ResolverError('File not found')
+    pattern = '(?://|\.)(gfsvideos\.com)/video/([\w\-]+)\.html'
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='http://www.{host}/video/{media_id}')
+        return self._default_get_url(host, media_id, template='http://www.{host}/video/{media_id}.html')
 
     @classmethod
     def _is_enabled(cls):
